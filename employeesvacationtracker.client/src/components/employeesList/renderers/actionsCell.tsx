3c926f2abf3084e8e61e9e42f0b9f15a5d9627c8
taha.seddik1992@gmail.com <tseddik@tikilearning.com>
2023-11-26 15:52:28 +0100

    table integration

---
+++ employeesvacationtracker.client/src/components/employeesList/renderers/actionsCell.tsx
@@ -0,0 +1,32 @@
+import { GridRowParams } from '@mui/x-data-grid';
+import { Box, IconButton, Tooltip } from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
+import { NavLink } from 'react-router-dom';
+import { RoutesMap } from '../../../routing/RoutesMap';
+
+interface AdditionalProps {
+  setOpenConfirm: (status: boolean) => void;
+}
+
+export const ActionsRendrer = ({ setOpenConfirm }: Partial<GridRowParams> & AdditionalProps) => {
+  const startDeleteProduct = () => {
+    setOpenConfirm(true);
+  };
+
+  return (
+    <Box display='flex' justifyContent='center' width='100%'>
+      <Tooltip title='Modifier le produit'>
+        <NavLink to={RoutesMap.editEmployee.path}>
+          <IconButton size='small' color='default' aria-label='edit'>
+            <Edit />
+          </IconButton>
+        </NavLink>
+      </Tooltip>
+      <Tooltip title='Supprimer le produit'>
+        <IconButton size='small' color='default' aria-label='delete' onClick={startDeleteProduct}>
+          <Delete />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  );
+};
--- employeesvacationtracker.client/src/pages/private/employees/employeesPage.tsx
+++ employeesvacationtracker.client/src/pages/private/employees/employeesPage.tsx
@@ -1,8 +1,14 @@
 import { styled } from '@mui/material/styles';
 import { ListingUpperBar } from '../../../components/common/ListingUpperBar';
-import { useEmployeesData } from '../../../components/employeesList/hooks';
+import { useEmployeesData } from '../../../components/employeesList/useEmployeesData';
+import { usePrepareEmployeesTableColumns } from '../../../components/employeesList/useTableConfig';
 import { RoutesMap } from '../../../routing/RoutesMap';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { IEmployee } from '../../../models/entities/employee';
+import ConfirmDialog from '../../../components/common/confirmDialog';
+import { DataGrid } from '@mui/x-data-grid/DataGrid';
+import { useState } from 'react';
 
 const Container = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(4),
@@ -13,7 +19,7 @@ const Container = styled(Paper)(({ theme }) => ({
 }));
 
 const EmployeesPage: React.FC<{}> = () => {
-  const { searchText, handleNewSearch, clearSearchTxt } = useEmployeesData();
+  const { searchText, employeesToShow, fetchRows, handleNewSearch, clearSearchTxt } = useEmployeesData();
 
   return (
     <Container className='fullySizedFlexColumn' elevation={3}>
@@ -25,8 +31,50 @@ const EmployeesPage: React.FC<{}> = () => {
         handleNewSearch={handleNewSearch}
         clearSearchTxt={clearSearchTxt}
       />
+      <Box flexGrow={1} mt={2}>
+        <EmployeesTable employees={employeesToShow} fetchRows={fetchRows} />
+      </Box>
     </Container>
   );
 };
+
+type ITableProps = {
+  employees: IEmployee[];
+  fetchRows: () => Promise<void>;
+};
+
+const EmployeesTable: React.FC<ITableProps> = ({ employees, fetchRows }) => {
+  const [openConfirm, setOpenConfirm] = useState(false);
+  const [selectedRowsIds, setSelectedRowIds] = useState<string[]>([]);
+
+  const { columns } = usePrepareEmployeesTableColumns(setOpenConfirm);
+
+  const confirmDeleteProduct = () => {};
+
+  return (
+    <Box display='flex' flexDirection='column' className='fullHW'>
+      <DataGrid
+        columns={columns}
+        rows={employees}
+        onRowSelectionModelChange={(rows) => setSelectedRowIds(rows as string[])}
+        rowSelectionModel={selectedRowsIds}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+      />
+      <ConfirmDialog
+        open={openConfirm}
+        title='Sure you wanna delete this employee ?'
+        onConfirm={() => confirmDeleteProduct()}
+        setOpen={setOpenConfirm}
+      />
+    </Box>
+  );
+};
+
+// rowHeight={80}
 
 export default EmployeesPage;
